c1715da329d9177f3e224ef1732f571a138d1640
4elovek37 <b.4elovek37@yandex.ru>
2020-03-31 20:56:28 +0300

    UI kit, basic site, map

--- analyzer/views.py
+++ analyzer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, '<h1>I am not ready!</h1>')
+    return render(request, 'index.html')
