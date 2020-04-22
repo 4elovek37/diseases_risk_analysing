4c8ab7d3932c6f84e1846a8b6d94d68cac8c43e5
bogdanvso <b.4elovek37@yandex.ru>
2020-04-22 20:48:35 +0300

    Settings preparation to deployment

--- diseases_risk_analysing/settings.py
+++ diseases_risk_analysing/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c&q8l28m^-mj%8gdp)9hbelpmms5e1nh3)y73)0ye0!ygw3pj9'
+#SECRET_KEY = 'c&q8l28m^-mj%8gdp)9hbelpmms5e1nh3)y73)0ye0!ygw3pj9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c&q8l28m^-mj%8gdp)9hbelpmms5e1nh3)y73)0ye0!ygw3pj9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
