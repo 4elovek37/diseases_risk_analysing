fc6b03cc3d97f07cb289670ad8bc4f612ab99da2
bogdanvso <b.4elovek37@yandex.ru>
2020-04-25 18:49:15 +0300

    temporarty test of scheduler [2]

--- updates_scheduler.py
+++ updates_scheduler.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', secondes=10)
+@sched.scheduled_job('interval', seconds=10)
 def timed_job():
     print('This job is run every 10 secondes.')
 
