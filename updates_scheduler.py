7642ebaea06788b6fbb6a232ca19f140baad2521
bogdanvso <b.4elovek37@yandex.ru>
2020-04-25 18:51:53 +0300

    remove temp scheduler code

--- updates_scheduler.py
+++ updates_scheduler.py
@@ -3,10 +3,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=10)
-def timed_job():
-    print('This job is run every 10 secondes.')
-
 
 @sched.scheduled_job('cron', hour=1)
 def scheduled_job():
