f35cf7974122056c871bad2389e3c439dcf59afc
bogdanvso <b.4elovek37@yandex.ru>
2020-04-25 18:54:16 +0300

    add logs to the scheduler

--- updates_scheduler.py
+++ updates_scheduler.py
@@ -6,7 +6,9 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=1)
 def scheduled_job():
-    call(['python', 'manage.py', 'handle_tasks'])
+    print('Calling handle_tasks...')
+    call(['python', './manage.py', 'handle_tasks'])
+    print('Calling handle_tasks done')
 
 
 sched.start()
